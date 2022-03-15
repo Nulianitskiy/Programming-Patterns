fe2c0cf719210cbb9382894ea6293b4ec04c2347
Nikita Ulyanitsky <nikitaul78@gmail.com>
2022-03-15 21:06:40 +0300

    L2.B2.E1,2

--- Department.rb
+++ Department.rb
@@ -52,3 +52,22 @@ class Department
        end
     end
 end
+
+def read_from_txt(path)
+    my_data = IO.read path
+    dept_list = Array.new
+    sub_data = Array.new
+    n = 0
+    my_data.split("\n").each do |i| 
+        if i != "///"
+            sub_data[n] = i 
+            n+=1
+        else 
+            n = 0
+            dept_list.push(Department.new(sub_data.shift, sub_data.shift, sub_data.shift.split("/")))
+        end
+    end
+    dept_list
+end
+
+
--- Main.rb
+++ Main.rb
@@ -1,11 +1,13 @@
 current_path = File.dirname(__FILE__)
 require "#{current_path}/Department.rb"
 
-dept1 = Department.new("NumberOne", "+71112345678", ["RABotat`","Slujit`","Prismikatsa"])
-dept2 = Department.new("BoomBigaBoom", "+80189091456", [])
+#dept1 = Department.new("NumberOne", "+71112345678", ["RABotat`","Slujit`","Prismikatsa"])
+#dept2 = Department.new("BoomBigaBoom", "+80189091456", [])
 
-puts dept1
-puts dept2
+#puts dept1
+#puts dept2
 
 #puts dept1.show_all_dutyes
 #puts dept2.show_all_dutyes
+
+puts read_from_txt("C:\\Users\\Captain\\Ruby\\Department.txt")
